6b7a1c90885d2a334fc27a8194c8c4e375b3a74a
salony pradhan <71633038+salonynhp@users.noreply.github.com>
2024-04-17 01:36:33 +0530

    Add files via upload

---
+++ EmployeeData/Controllers/EmployeeController.cs
@@ -0,0 +1,52 @@
+﻿
+
+//using Employee.Business.Contract;
+using Employee.Business;
+//using Employee.Business.Business;
+using Employee.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeData.Controllers
+{   
+    //This controller will handle the HTTP requests related to employees.
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+      
+        private readonly EmployeeBusiness _EmployeeBusiness;
+        public EmployeeController()
+        {
+            _EmployeeBusiness = new EmployeeBusiness();
+        }
+
+        //change start
+        
+        //////1.creation of endpoint
+        [HttpGet("{id}")]
+        public ActionResult<EmployeeMaster> GetEmployeeById(int id)
+        {
+            var employee = _EmployeeBusiness.GetEmployeeById(id);
+            if (employee == null)
+                return NotFound();
+                
+           return Ok(employee);
+        } 
+        //change end
+        /*
+        //action method with parameter
+        [HttpGet("{id}")]
+        public string GetEmployeeById(int id)
+        {
+            return employee.ElementAt(id);
+        }
+        */
+        [HttpGet()]
+        public IEnumerable<EmployeeMaster> Getemployee()
+        {
+            var result = _EmployeeBusiness.GetEmployee();
+            return result;
+        }
+    }
+}
---
+++ EmployeeData/WeatherForecast.cs
@@ -0,0 +1,14 @@
+namespace EmployeeData
+{
+    //this is a model given by default
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
