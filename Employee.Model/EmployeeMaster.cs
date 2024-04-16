d10cb479a7b46c4ff6ae1c19ca22ac8584f26e46
salony pradhan <71633038+salonynhp@users.noreply.github.com>
2024-04-17 01:37:52 +0530

    Add files via upload

---
+++ Employee.Model/EmployeeMaster.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.Model
+{
+    //3.Create a Models: (DTOs) EmployeeMaster.cs
+    public class EmployeeMaster
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime JoinDate { get; set; }
+    }
+}
